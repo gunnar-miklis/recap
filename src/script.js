1939c80f8954c4fd95a4665449071f31f00ca935
Gunnar Miklis <gunnar.miklis@gmail.com>
2024-03-29 18:28:02 +0100

    session 3

--- src/script.js
+++ src/script.js
@@ -156,7 +156,6 @@ console.log(isOddOrEven(4));
 console.log(isOddOrEven(1));
 console.log(isOddOrEven(0));
 
-
 // session 2
 
 // NOTE: arrays
@@ -246,3 +245,97 @@ console.log(winterAndSpringSpreaded); // ['dec', 'jan', 'feb', 'mar', 'apr', 'ma
 
 const highestNumber = Math.max(...randomNumbers); // 45
 const lowestNumber = Math.min(...randomNumbers); // 4
+
+// session 3
+
+// NOTE: objects
+obj; // { first: 1, second: 2 }
+console.log(obj.first); // 1
+console.log(obj['first']); // 1
+
+obj.third = 3; // { first: 1, second: 2, third: 3 }
+obj['fourth'] = 4; // { first: 1, second: 2, third: 3, fourth: 4 }
+delete obj.fourth; // { first: 1, second: 2, third: 3 }
+
+Object.keys(obj).forEach((key) => {
+	console.log(key); // 'first', 'second', 'third'
+});
+Object.values(obj).forEach((value) => {
+	console.log(value); // 1, 2, 3
+});
+
+// NOTE: nested structures
+const objInArray = [
+	{ name: 'Bob', age: 17 },
+	{ name: 'Sara', age: 19 },
+	{ name: 'Ted', age: 22 },
+	{ name: 'Mari', age: 24 },
+];
+console.log(objInArray[1].name); // Sara
+console.log(objInArray[3].age); // 24
+
+const arrayInObj = {
+	Cities: ['Berlin', 'Paris', 'London'],
+	Students: [
+		{ name: 'Bob', age: 17 },
+		{ name: 'Sara', age: 19 },
+		{ name: 'Ted', age: 22 },
+		{ name: 'Mari', age: 24 },
+	],
+};
+console.log(arrayInObj.Students[1].name); // Sara
+console.log(arrayInObj.Students[3].age); // 24
+console.log(arrayInObj.Cities[0]); // Berlin
+
+const arrayInArray = [
+	['red', 'green', 'blue'],
+	['sweet', 'sour', 'bitter'],
+	['sunny', 'cloudy', 'rainy'],
+];
+console.log(arrayInArray[1][0]); // sweet
+
+const functionInObj = {
+	firstName: 'John',
+	lastName: 'Doe',
+	fullName: function () {
+		return `${this.firstName} ${this.lastName}`;
+	},
+};
+console.log(functionInObj.fullName()); // John Doe
+const functionInObj2 = {
+	firstName: 'John',
+	lastName: 'Doe',
+	fullName: () => `${functionInObj2.firstName} ${functionInObj2.lastName}`,
+};
+console.log(functionInObj2.fullName()); // John Doe
+
+const objMethods = {
+	firstName: 'John',
+	lastName: 'Doe',
+	age: 33,
+	gender: 'male',
+	money: 10,
+	turnover: 0,
+	displayUserInfo() {
+		return `${this.firstName} ${this.lastName} is ${this.age} years old. ${
+			this.gender === 'male' ? 'He' : 'She'
+		} has ${this.money} money.`;
+	},
+	increaseMoney(amount) {
+		this.money += amount;
+		this.turnover += amount;
+	},
+	decreaseMoney(amount) {
+		this.money -= amount;
+		this.turnover -= amount;
+	},
+};
+console.log(objMethods.displayUserInfo()); // 'John Doe is 33 years old. He has 10 money'
+objMethods.increaseMoney(50);
+objMethods.decreaseMoney(20);
+console.log(objMethods.displayUserInfo()); // 'John Doe is 33 years old. He has 40 money'
+console.log(objMethods.turnover); // 30
+
+// TODO: classes
+
+
