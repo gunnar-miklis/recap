ae0c0cf3ba911f3d59d3ce57bfb728709af188e2
Gunnar Miklis <gunnar.miklis@gmail.com>
2024-04-22 18:18:33 +0200

    session 24

---
+++ src/frontend/axios/script.js
@@ -0,0 +1,28 @@
+const apiURL = 'https://restcountries.com/v3.1/name/';
+
+// fetch API
+async function getData(url, input) {
+	try {
+		const apiResponse = await axios.get(
+			`${url}${input ? input : 'deutschland'}`,
+		);
+		const apiData = await apiResponse.data[0];
+		return [apiData.capital[0], apiData.cca2];
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+// DOM
+const button = document.querySelector('button');
+button.addEventListener('click', async () => {
+	try {
+		const inputTag = document.querySelector('input');
+		const data = await getData(apiURL, inputTag.value);
+
+		const paragraph = document.querySelector('p');
+		paragraph.innerText = `${data[1]}: ${data[0]}`;
+	} catch (err) {
+		console.error(err);
+	}
+});
--- src/password-encryption/routes/authentication.js
+++ src/password-encryption/routes/authentication.js
@@ -4,6 +4,13 @@ import UserModel from '../db/schema.js';
 import bcrypt from 'bcryptjs';
 const saltRounds = 10;
 
+// COMMENT: 
+//	* Authentication: process of verifiying the identity (providing credentials, proving your identity)
+//	* Verification: process of confirming the truthfulness (matching credentials, confirming accuracy of that proof)
+//	* Validation: process of checking whether something meets certain criteria/standards/rules (password: number, lowercase, uppercase, length, etc.)
+//	* Authorization: process of determining what actions a user is allowed to access (guest, user, admin, etc.)
+
+
 // NOTE: verify  user
 router.post('/users/auth', async (req, res, next) => {
 	const user = req.body;
